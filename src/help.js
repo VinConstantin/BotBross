0bf2e302ee94a8e7ce6fcad2db4ab7e9fe2bfa7e
charliesis <charlie_23@live.ca>
2018-01-29 19:36:18 -0500

    fixed help

--- src/help.js
+++ src/help.js
@@ -1,17 +1,19 @@
 exports.help = function() {
     var msg = `!roll {range} 
-    \n!flip 
-    \n!reee 
-    \n!weather {city} 
-    \n!ranked {summoner_id} 
-    \n!bing \n!day {location} 
-    \n!rl {ranked; stats} {steamId} 
-    \n!steam {played} {steamId} 
-    \n!play \n!skip 
-    \n!stop 
-    \n!list
-    \n!pause
-    \n!resume
+!flip 
+!reee 
+!weather {city} 
+!ranked {summoner_id} 
+!bing 
+!day {location} 
+!rl {ranked; stats} {steamId} 
+!steam {played} {steamId} 
+!play 
+!skip 
+!stop 
+!list
+!pause
+!resume
     `
     return msg;
 }
